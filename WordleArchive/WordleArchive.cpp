1a2c353cb63416ab58663de70f54580c186fc647
arkel <arkel@ARKEL-DELL>
2022-05-27 18:27:42 -0500

    Add project files.

---
+++ WordleArchive/WordleArchive.cpp
@@ -0,0 +1,12 @@
+﻿// WordleArchive.cpp : Defines the entry point for the application.
+//
+
+#include "WordleArchive.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
